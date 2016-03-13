37f86c7c1ba3590d24c98e66cfd0671f11ba0edc
CookieShade <CookieShade@users.noreply.github.com>
2016-03-13 17:22:36 +0100

    Open block braces on new line
    Fixed the coding style to suit my personal preference. Object literals
    don't have brace on new line, to prevent semicolon insertion and
    separate them from blocks.
    
    No idea why it was changed in the first place :/

--- BETA.js
+++ BETA.js
@@ -6,19 +6,22 @@
   comments in swedish are the dev's notes to himself
 */
 
-(function () {
+(function ()
+{
     "use strict";
     window.BETA = {};
 
     BETA.colorProto = {
-        toString: function () {
+        toString: function ()
+        {
             return (this.a >= 1) ?
                 "rgb(" + this.r + "," + this.g + "," + this.b + ")" :
                 "rgba(" + this.r + "," + this.g + "," + this.b + "," + this.a + ")";
         }
     };
 
-    BETA.rgba = function (red, green, blue, alpha) {
+    BETA.rgba = function (red, green, blue, alpha)
+    {
         var color = Object.create(BETA.colorProto);
         color.r = red;
         color.g = green;
@@ -35,7 +38,8 @@
         }
     };
 
-    BETA.vector = function (x, y) {
+    BETA.vector = function (x, y)
+    {
         //Vectors inherit .toString() from vProto
         var v = Object.create(BETA.vProto);
         v.x = x;
@@ -45,45 +49,55 @@
 
     BETA.v = BETA.vector;
 
-    BETA.vStringify = function (v) {
+    BETA.vStringify = function (v)
+    {
         return "(" + v.x + ", " + v.y + ")";
     };
 
-    BETA.vAdd = function (v1, v2) {
+    BETA.vAdd = function (v1, v2)
+    {
         return BETA.v(v1.x + v2.x, v1.y + v2.y);
     };
 
-    BETA.vSubtract = function (v1, v2) {
+    BETA.vSubtract = function (v1, v2)
+    {
         return BETA.v(v1.x - v2.x, v1.y - v2.y);
     };
 
-    BETA.vScale = function (v1, v2) {
+    BETA.vScale = function (v1, v2)
+    {
         return BETA.v(v1.x * v2.x, v1.y * v2.y);
     };
 
-    BETA.vDot = function (v1, v2) {
+    BETA.vDot = function (v1, v2)
+    {
         return v1.x * v2.x + v1.y * v2.y;
     };
 
-    BETA.vScalarMult = function (v, s) {
+    BETA.vScalarMult = function (v, s)
+    {
         return BETA.v(v.x * s, v.y * s);
     };
 
-    BETA.vScalarDiv = function (v, s) {
+    BETA.vScalarDiv = function (v, s)
+    {
         return BETA.v(v.x / s, v.y / s);
     };
 
-    BETA.vMagnitude = function (v) {
+    BETA.vMagnitude = function (v)
+    {
         return Math.sqrt(v.x * v.x + v.y * v.y);
     };
 
-    BETA.vNormalize = function (v) {
+    BETA.vNormalize = function (v)
+    {
         return (v.x === 0 && v.y === 0) ? v : BETA.vScalarDiv(v, BETA.vMagnitude(v));
     };
 
     //------------CANVAS INTERFACE------------\\
 
-    BETA.Canvas = function (id) {
+    BETA.Canvas = function (id)
+    {
         var cvs = document.getElementById(id);
         var ctx = cvs.getContext("2d");
 
@@ -95,7 +109,8 @@
         this.sizeVector = BETA.v(cvs.width, cvs.height);
     };
 
-    BETA.Canvas.prototype.resize = function (x, y) {
+    BETA.Canvas.prototype.resize = function (x, y)
+    {
         this.width = x;
         this.height = y;
         this.sizeVector = BETA.v(x, y);
@@ -105,7 +120,8 @@
         this.canvas.style.height = y + "px";
     };
 
-    BETA.Canvas.prototype.vectorResize = function (vector) {
+    BETA.Canvas.prototype.vectorResize = function (vector)
+    {
         this.width = vector.x;
         this.height = vector.y;
         this.sizeVector = BETA.v(vector.x, vector.y);
@@ -115,7 +131,8 @@
         this.canvas.style.height = vector.y + "px";
     };
 
-    BETA.Canvas.prototype.line = function (posA, posB, thickness, style) {
+    BETA.Canvas.prototype.line = function (posA, posB, thickness, style)
+    {
         this.context.strokeStyle = style;
         this.context.lineWidth = thickness;
         this.context.beginPath();
@@ -125,46 +142,56 @@
         this.context.closePath();
     };
 
-    BETA.Canvas.prototype.rect = function (pos, size, style) {
+    BETA.Canvas.prototype.rect = function (pos, size, style)
+    {
         this.context.fillStyle = style;
         this.context.fillRect(pos.x, pos.y, size.x, size.y);
     };
 
-    BETA.Canvas.prototype.lineRect = function (pos, size, thickness, style) {
+    BETA.Canvas.prototype.lineRect = function (pos, size, thickness, style)
+    {
         this.context.strokeStyle = style;
         this.context.lineWidth = thickness;
         this.context.strokeRect(pos.x, pos.y, size.x, size.y);
     };
 
-    BETA.Canvas.prototype.translate = function (x, y) {
+    BETA.Canvas.prototype.translate = function (x, y)
+    {
         this.context.translate(x, y);
     };
 
-    BETA.Canvas.prototype.vectorTranslate = function (vector) {
+    BETA.Canvas.prototype.vectorTranslate = function (vector)
+    {
         this.context.translate(vector.x, vector.y);
     };
 
-    BETA.Canvas.prototype.scale = function (x, y) {
+    BETA.Canvas.prototype.scale = function (x, y)
+    {
         this.context.scale(x, y);
     };
 
-    BETA.Canvas.prototype.vectorScale = function (vector) {
+    BETA.Canvas.prototype.vectorScale = function (vector)
+    {
         this.context.scale(vector.x, vector.y);
     };
 
-    BETA.Canvas.prototype.rotate = function (degrees) {
+    BETA.Canvas.prototype.rotate = function (degrees)
+    {
         this.context.rotate(degrees * Math.PI / 180);
     };
 
-    BETA.Canvas.prototype.rotateRad = function (radians) {
+    BETA.Canvas.prototype.rotateRad = function (radians)
+    {
         this.context.rotate(radians);
     };
 
-    BETA.Canvas.prototype.save = function () {
+    BETA.Canvas.prototype.save = function ()
+    {
         this.context.save();
     };
 
-    BETA.Canvas.prototype.restore = function () {
+    BETA.Canvas.prototype.restore = function ()
+    {
         this.context.restore();
     };
 
