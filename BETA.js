b26cb270fd6487c8cda0712def2194ad8b9b1f3a
CookieShade <CookieShade@users.noreply.github.com>
2016-08-07 15:55:08 +0200

    Check if getRenderer gets a canvas ID

--- BETA.js
+++ BETA.js
@@ -368,6 +368,7 @@
     BETA.getRenderer = function (id)
     {
         var canvas = document.getElementById(id);
+        BETA.assert(canvas instanceof HTMLCanvasElement, "getRenderer(): Element #" + id + " is not a <canvas>");
         var context = canvas.getContext("2d");
 
         var renderer = Object.create(canvasRendererProto);
