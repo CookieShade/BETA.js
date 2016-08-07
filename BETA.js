b0ae37aa0b48a5c998e1defcf96ad2f066bdfe17
CookieShade <CookieShade@users.noreply.github.com>
2016-08-07 12:27:46 +0200

    Add vComplexMult

--- BETA.js
+++ BETA.js
@@ -246,6 +246,14 @@
         return v1.x * v2.x + v1.y * v2.y;
     };
 
+    BETA.vComplexMult = function (v1, v2)
+    {
+        return {
+            x: v1.x * v2.x - v1.y * v2.y,
+            y: v1.x * v2.y + v2.x * v1.y
+        };
+    };
+
     BETA.vScalarMult = function (v, s)
     {
         return {
